99cf22211445a18519faa98d66198706baf6bfd9
Devin Weaver <suki@tritarget.org>
2018-05-02 12:13:06 -0400

    Add ember-cli-deploy pipeline

--- config/environment.js
+++ config/environment.js
@@ -46,7 +46,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.rootURL = '/';
+    ENV.rootURL = '/test-app/';
     ENV.locationType = 'hash';
   }
 
