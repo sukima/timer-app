f837a823886afc26cdb20e9a648922e15218cbff
Devin Weaver <suki@tritarget.org>
2018-05-02 12:37:37 -0400

    Update config for use with gh-pages CNAME

--- config/environment.js
+++ config/environment.js
@@ -46,7 +46,6 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.rootURL = '/timer-app/';
     ENV.locationType = 'hash';
   }
 
