8465d8967d43842ceacf6acdfeadbe8ac2566004
Devin Weaver <devin.weaver@crowdstrike.com>
2019-06-20 12:09:10 -0400

    Deploy dist files by ember-cli-deploy-ghpages

---
+++ dist/assets/tests.js
@@ -0,0 +1,241 @@
+'use strict';
+
+define('timer-app/tests/app.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | app');
+
+  QUnit.test('app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('components/context-menu.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'components/context-menu.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('components/main-nav.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'components/main-nav.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('components/time-picker.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'components/time-picker.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('components/timer-display.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'components/timer-display.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('components/top-bar.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'components/top-bar.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('controllers/about.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'controllers/about.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('controllers/application.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'controllers/application.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('controllers/index.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'controllers/index.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('controllers/settings.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'controllers/settings.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('mixins/timer-class.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'mixins/timer-class.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('resolver.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'resolver.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('router.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('routes/index.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/index.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('services/timer.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'services/timer.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('utils/parse-time.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'utils/parse-time.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('utils/timer-states.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'utils/timer-states.js should pass ESLint\n\n');
+  });
+});
+define('timer-app/tests/helpers/destroy-app', ['exports'], function (exports) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = destroyApp;
+  function destroyApp(application) {
+    Ember.run(application, 'destroy');
+  }
+});
+define('timer-app/tests/helpers/ember-keyboard/register-test-helpers', ['exports', 'ember-keyboard', 'ember-keyboard/fixtures/modifiers-array', 'ember-keyboard/fixtures/mouse-buttons-array', 'ember-keyboard/utils/get-cmd-key'], function (exports, _emberKeyboard, _modifiersArray, _mouseButtonsArray, _getCmdKey) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  exports.default = function () {
+    Ember.Test.registerAsyncHelper('keyDown', function (app, attributes, element) {
+      return keyEvent(app, attributes, 'keydown', element);
+    });
+
+    Ember.Test.registerAsyncHelper('keyUp', function (app, attributes, element) {
+      return keyEvent(app, attributes, 'keyup', element);
+    });
+
+    Ember.Test.registerAsyncHelper('keyPress', function (app, attributes, element) {
+      return keyEvent(app, attributes, 'keypress', element);
+    });
+
+    Ember.Test.registerAsyncHelper('mouseDown', function (app, attributes, element) {
+      return keyEvent(app, attributes, 'mousedown', element);
+    });
+
+    Ember.Test.registerAsyncHelper('mouseUp', function (app, attributes, element) {
+      return keyEvent(app, attributes, 'mouseup', element);
+    });
+
+    Ember.Test.registerAsyncHelper('touchStart', function (app, attributes, element) {
+      return keyEvent(app, attributes, 'touchstart', element);
+    });
+
+    Ember.Test.registerAsyncHelper('touchEnd', function (app, attributes, element) {
+      return keyEvent(app, attributes, 'touchend', element);
+    });
+  };
+
+  var keyEvent = function keyEvent(app, attributes, type, element) {
+    var event = (attributes || '').split('+').reduce(function (event, attribute) {
+      if (_modifiersArray.default.indexOf(attribute) > -1) {
+        attribute = attribute === 'cmd' ? (0, _getCmdKey.default)() : attribute;
+        event[attribute + 'Key'] = true;
+      } else if (_mouseButtonsArray.default.indexOf(attribute) > -1) {
+        event.button = (0, _emberKeyboard.getMouseCode)(attribute);
+      } else {
+        event.keyCode = (0, _emberKeyboard.getKeyCode)(attribute);
+      }
+
+      return event;
+    }, {});
+
+    return app.testHelpers.triggerEvent(element || document.body, type, event);
+  };
+});
+define('timer-app/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'timer-app/tests/helpers/start-app', 'timer-app/tests/helpers/destroy-app'], function (exports, _qunit, _startApp, _destroyApp) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  exports.default = function (name) {
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+    (0, _qunit.module)(name, {
+      beforeEach: function beforeEach() {
+        this.application = (0, _startApp.default)();
+
+        if (options.beforeEach) {
+          return options.beforeEach.apply(this, arguments);
+        }
+      },
+      afterEach: function afterEach() {
+        var _this = this;
+
+        var afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+        return Ember.RSVP.resolve(afterEach).then(function () {
+          return (0, _destroyApp.default)(_this.application);
+        });
+      }
+    });
+  };
+});
+define('timer-app/tests/helpers/start-app', ['exports', 'timer-app/app', 'timer-app/config/environment'], function (exports, _app, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = startApp;
+  function startApp(attrs) {
+    var attributes = Ember.merge({}, _environment.default.APP);
+    attributes.autoboot = true;
+    attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+
+    return Ember.run(function () {
+      var application = _app.default.create(attributes);
+      application.setupForTesting();
+      application.injectTestHelpers();
+      return application;
+    });
+  }
+});
+define('timer-app/tests/test-helper', ['timer-app/app', 'timer-app/config/environment', '@ember/test-helpers', 'ember-qunit'], function (_app, _environment, _testHelpers, _emberQunit) {
+  'use strict';
+
+  (0, _testHelpers.setApplication)(_app.default.create(_environment.default.APP));
+
+  (0, _emberQunit.start)();
+});
+define('timer-app/tests/tests.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | tests');
+
+  QUnit.test('helpers/destroy-app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/destroy-app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/module-for-acceptance.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/start-app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/start-app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('test-helper.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass ESLint\n\n');
+  });
+});
+require('timer-app/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
